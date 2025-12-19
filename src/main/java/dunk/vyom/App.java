507d9c5cda92096c651d8aff47344c6930d6e392
Milton Ytusaca <milton.yv98@gmail.com>
2025-12-19 11:36:13 -0500

    organizacion de carpetas

--- src/main/java/dunk/vyom/App.java
+++ src/main/java/dunk/vyom/App.java
@@ -2,13 +2,16 @@ package dunk.vyom;
 
 import java.sql.Connection;
 import dunk.vyom.config.Database;
+import dunk.vyom.ui.MenuUI;
 
 public class App {
   public static void main(String[] args) {
-    try (Connection con = Database.getConnection()) {
-      System.out.println("Conectado a la DB!!" + !con.isClosed());
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    // try (Connection con = Database.getConnection()) {
+    // System.out.println("Conectado a la DB!!" + !con.isClosed());
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    MenuUI men = new MenuUI();
+    men.loop();
   }
 }
--- src/main/java/dunk/vyom/ui/MenuUI.java
+++ src/main/java/dunk/vyom/ui/MenuUI.java
@@ -10,4 +10,27 @@ public class MenuUI {
             ██     ██▄▄▄ ██ ██ ▀███▀   ████▀  ▀███▀ ██ ▀██ ▀███▀ ██▄▄▄ ▀███▀ ██ ▀██
       """;
 
+  public void loop() {
+    while (true) {
+      System.out.println("Dungeon Matrix");
+      System.out.println("1) Iniciar Sesion");
+      System.out.println("2) Registrarse");
+      System.out.println("3) Cambiar idioma");
+      System.out.println("4) Salir");
+
+      System.out.println();
+      String opt = sc.nextLine().trim();
+
+      switch (opt) {
+        case "1" -> System.out.println("Iniciando sesion...");
+        case "2" -> System.out.println("Iniciando registro...");
+        case "3" -> System.out.println("No hay mas idiomas XD");
+        case "4" -> {
+          System.out.println("Saliendo del dungeon...");
+          return;
+        }
+        default -> System.out.println("Opcion invalida...");
+      }
+    }
+  }
 }
